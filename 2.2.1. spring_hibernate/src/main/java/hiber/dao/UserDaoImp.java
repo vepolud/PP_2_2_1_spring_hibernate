44726187b34a719741d21967aeaa42601a870d41
vepolud <vepolud@gmail.com>
2022-05-30 17:05:57 +0300

    убрал лишние аннотации транзактионал. Добавил Аннотацию транзактионал в дао. без ней почему то не работало

--- 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
@@ -5,10 +5,13 @@ import hiber.model.User;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
 import javax.persistence.TypedQuery;
 import java.util.List;
 
 @Repository
+@Transactional
 public class UserDaoImp implements UserDao {
 
    private SessionFactory sessionFactory;
