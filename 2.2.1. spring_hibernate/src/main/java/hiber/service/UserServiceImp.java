644437c93e762f9fbd9e8785eaf4c4d7e20bc1fd
vepolud <vepolud@gmail.com>
2022-06-07 19:13:21 +0300

    Убрал транзакции из репо, поменял sessionFactory на EntityManager

--- 2.2.1. spring_hibernate/src/main/java/hiber/service/UserServiceImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/service/UserServiceImp.java
@@ -7,39 +7,42 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.util.List;
 
 @Service
 public class UserServiceImp implements UserService {
 
-   private UserDao userDao;
 
-   @Autowired
-   public UserServiceImp(UserDao userDao) {
-      this.userDao = userDao;
-   }
+    private UserDao userDao;
 
-   @Transactional
-   @Override
-   public void add(User user) {
-      userDao.add(user);
-   }
+    @Autowired
+    public UserServiceImp(UserDao userDao) {
+        this.userDao = userDao;
+    }
 
-   @Transactional(readOnly = true)
-   @Override
-   public List<User> listUsers() {
-      return userDao.listUsers();
-   }
+    @Transactional
+    @Override
+    public void add(User user) {
+        userDao.add(user);
+    }
 
-   @Transactional
-   @Override
-   public void addCar(Car car) {
-      userDao.addCar(car);
-   }
 
-   @Transactional(readOnly = true)
-   @Override
-   public User getUser(String model, int series) {
-      return userDao.getUser(model, series);
-   }
+    @Override
+    public List<User> listUsers() {
+        return userDao.listUsers();
+    }
+
+    @Transactional
+    @Override
+    public void addCar(Car car) {
+        userDao.addCar(car);
+    }
+
+
+    @Override
+    public User getUser(String model, int series) {
+        return userDao.getUser(model, series);
+    }
 }
